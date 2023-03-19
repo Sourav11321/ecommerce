e68ea3adaf87fe85f53f642d0427ad86a4a8dd5e
Sourav Bandyopadhyay <124033219+Sourav11321@users.noreply.github.com>
2023-03-19 15:59:57 +0530

    Add files via upload

---
+++ mainapp/views.py
@@ -0,0 +1,118 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .models import Post
+from .forms import SignUpForm,LoginForm,PostForm
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+
+
+
+def homepage(request) :
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts' : posts})
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+     posts = Post.objects.all()
+     user = request.user
+     full_name = user.get_full_name()
+     return render(request, 'dashboard.html',{'posts':posts,'full_name':full_name})
+    else:
+      return HttpResponseRedirect('/login/')
+
+
+def contact(request) :
+    return render(request, 'contact.html')
+
+
+
+def about(request) :
+    return render(request, 'about.html')
+
+def profile(request) :
+    return render(request, 'profile.html')
+
+def address(request):
+ return render(request, 'address.html')
+
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Registered Successfully')
+            form.save()
+    else :
+        form = SignUpForm()
+    return render(request, 'sign_up.html',{'form':form})
+
+
+def user_login(request):
+ if not request.user.is_authenticated:
+    if request.method == 'POST':
+      form = LoginForm(request=request,data=request.POST)
+      if form.is_valid():
+        uname = form.cleaned_data['username']
+        upass = form.cleaned_data['password']
+        user = authenticate(username=uname,password=upass)
+        if user is not None:
+         login(request,user)
+         messages.success(request,'Logged in successfully')
+         return HttpResponseRedirect('/dashboard/')
+    else:
+      form = LoginForm()
+    return render(request, 'login.html',{'form':form})
+ else:
+  return HttpResponseRedirect('/dashboard/')
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def addpost(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid:
+                messages.success(request,'Addpost Successfully')
+                form.save()
+                form = PostForm()
+        else:
+            form = PostForm()
+        return render(request,'addpost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+def updatepost(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk=id)
+            form = PostForm(request.POST,instance=pi)
+            if form.is_valid():
+                messages.success(request,'Updatepost Successfully')
+                form.save()
+        else:
+            pi = Post.objects.get(pk=id)
+            form = PostForm(instance=pi)
+        return render(request,'updatepost.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+       
+
+def deletepost(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk=id)
+            pi.delete()
+
+
+            posts = Post.objects.all()
+            user = request.user
+            full_name = user.get_full_name()
+
+        return render(request, 'dashboard.html',{'posts':posts, 'full_name':full_name})
+    else:
+        return HttpResponseRedirect('/login/')
